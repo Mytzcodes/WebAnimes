cdcd371571fec5c5aa8af575811b45a1e009b78d
Mytzcodes <159034746+Mytzcodes@users.noreply.github.com>
2024-09-01 09:39:53 +0700

    Update instruk.js
    Add blur bright

--- instruk.js
+++ instruk.js
@@ -1,14 +1,20 @@
 const Btn_Actived = document.querySelector('.Activs');
+const contrased = document.querySelector('.sects');
+
 const show = document.getElementById('shown');
+
 
 function Showing() {
     show.style.display = 'block';
     show.style.transition = '5s';
+    contrased.style.filter = 'brightness(50%)';
+
 } 
 
 function Closing() {
     show.style.display = 'none';
     show.style.transition = '5s';
+    contrased.style.filter = 'brightness(100%)';
 } 
 
 

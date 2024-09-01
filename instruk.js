f955eaf0683245e89fa37da5c814cd1e6a58667d
Mytzcodes <159034746+Mytzcodes@users.noreply.github.com>
2024-09-01 08:11:45 +0700

    Add files via upload

---
+++ instruk.js
@@ -0,0 +1,18 @@
+const Btn_Actived = document.querySelector('.Activs');
+const show = document.getElementById('shown');
+
+function Showing() {
+    show.style.display = 'block';
+    show.style.transition = '5s';
+} 
+
+function Closing() {
+    show.style.display = 'none';
+    show.style.transition = '5s';
+} 
+
+
+
+    
+    
+    
